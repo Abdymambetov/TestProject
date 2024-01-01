27b11140c7dfe119568371c85b3d6b9ba7307672
Abdymambetov <meder0555@icloud.com>
2024-01-01 22:18:18 +0600

    add rewiews and rating

--- main/models.py
+++ main/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
+    tags= models.ManyToManyField(Tag, blank=True)
     title = models.CharField(max_length=100)
     text = models.TextField(null=True, blank=True)
     price = models.FloatField(default=0)
@@ -15,4 +29,38 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+    @property
+    def filtered_reviews(self):
+        return self.reviews.filter(stars__gte=4)
+    
+    @property
+    def rating(self):
+        reviews = self.filtered_reviews
+        count = reviews.count()
+        total = 0
+        for i in reviews:
+            total += i.stars
+        try:
+            return total / count
+        except ZeroDivisionError:
+            return 0
+    
 
+
+
+
+STAR_CHOISE = (
+    (1,'*'),
+    (2,'* *'),
+    (3,'* * *'),
+    (4,'* * * *'),
+    (5,'* * * * *'),
+)
+class Review(models.Model):
+    text = models.TextField()
+    stars = models.IntegerField(choices=STAR_CHOISE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return self.text
--- main/serializers.py
+++ main/serializers.py
@@ -1,10 +1,41 @@
 from rest_framework import serializers
-from main.models import Product
+from main.models import Product, Category, Tag, Review
 
+
+
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model= Category
+        fields = 'id name'.split()
+
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    class Meta:
+        model= Review
+        fields = 'id text stars'.split()
+
+# class TagsSerializers(serializers.ModelSerializer):
+#     class Meta:
+#         model= Tag
+#         fields = 'id name'.split()
 
 class ProductSerializers(serializers.ModelSerializer):
+    category = CategorySerializers()
+    tags = serializers.SerializerMethodField()
+    filtered_reviews = ReviewSerializers(many=True)
     class Meta:
         model = Product
-        # fields = ['id', 'title', 'text', 'price']
-        fields = '__all__'
-        # fields = 'id title price quantity'.split()
+        fields = ['id', 'title', 'text', 'price']
+        # fields = '__all__'
+        fields = 'id title category tags price quantity filtered_reviews rating'.split()
+
+    def get_tags(self, product):
+        # первый способ:
+        # list_ = []
+        # for tag in product.tags.all():
+        #     list_.append(tag.name)
+        # return list_
+        # второй способ:
+        return [tag.name for tag in product.tags.all()]
